3b564c94b3027050c9f6a6e3459962fe869e33d7
Mamdouh Morad <mamdouh93morad@gmail.com>
2022-11-24 05:02:19 +0200

    remove material ui and use basic html/css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatGridListModule} from '@angular/material/grid-list';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -12,14 +11,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {  MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import {  MatButtonModule } from '@angular/material/button';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import {MatListModule} from '@angular/material/list'
-import {MatIconModule} from '@angular/material/icon';
 import { ProductComponent } from './product/product.component'
-import {MatCardModule} from '@angular/material/card'
-import {MatSidenavModule} from '@angular/material/sidenav'
-import {MatBadgeModule} from '@angular/material/badge'
+
 
 @NgModule({
   declarations: [
@@ -37,16 +31,9 @@ import {MatBadgeModule} from '@angular/material/badge'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatGridListModule,
     FormsModule,
     MatFormFieldModule,
-    HttpClientModule,
-    MatButtonModule,
-    MatListModule,
-    MatIconModule,
-    MatCardModule,
-    MatSidenavModule,
-    MatBadgeModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
